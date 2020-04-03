d595bb1f2799e6c7a09ef18ab98b5a307b7c5a36
Anniina Korpinen <anniina.korpinen@fmi.fi>
2020-04-03 15:23:59 +0300

    initialized test functions

--- tests/unit/test_generate_grid_product.py
+++ tests/unit/test_generate_grid_product.py
@@ -1,4 +1,11 @@
 import numpy as np
+from model_evaluation.products.generate_grid_product import ObservationManager
+from model_evaluation.products.model_products import ModelGrid
+
+
+MODEL = 'ecmwf'
+OUTPUT_FILE = '/home/korpinen/Documents/ACTRIS/model_evaluation/test_data_ecmwf.nc'
+PRODUCT = 'iwc'
 
 
 class CategorizeBits:
@@ -12,10 +19,10 @@ class CategorizeBits:
                               'melting': np.asarray([[1, 0, 1, 0, 0, 0],
                                                      [1, 1, 0, 0, 0, 0]], dtype=bool),
                               'aerosol': np.asarray([[1, 0, 1, 0, 0, 0],
-                                                      [0, 0, 0, 0, 0, 0]], dtype=bool),
+                                                     [0, 0, 0, 0, 0, 0]], dtype=bool),
                               'insect': np.asarray([[1, 1, 0, 0, 0, 0],
-                                                     [0, 0, 1, 0, 0, 0]], dtype=bool),
-                               }
+                                                    [0, 0, 1, 0, 0, 0]], dtype=bool),
+                              }
         self.quality_bits = {'radar': np.asarray([[0, 0, 0, 1, 1, 1],
                                                   [1, 0, 0, 1, 1, 1]], dtype=bool),
                              'lidar': np.asarray([[1, 1, 1, 1, 0, 0],
@@ -28,3 +35,31 @@ class CategorizeBits:
                                                        [0, 1, 1, 0, 0, 0]], dtype=bool),
                              'corrected': np.asarray([[1, 0, 0, 0, 0, 0],
                                                       [1, 1, 0, 0, 0, 0]], dtype=bool)}
+
+
+def test_regridded_array():
+    assert True
+
+
+def test_time2datetime():
+    assert True
+
+
+def test_get_date():
+    assert True
+
+
+def test_generate_product():
+    assert True
+
+
+def test_generate_cv():
+    assert True
+
+
+def test_get_rainrate_threshold():
+    assert True
+
+
+def test_rain_index():
+    assert True
