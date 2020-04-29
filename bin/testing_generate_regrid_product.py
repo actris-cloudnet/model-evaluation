5c9af5da28d899293b521a5f4c0cf470d3799c07
Anniina Korpinen <anniina.korpinen@fmi.fi>
2020-04-29 17:02:03 +0300

    issue might appear with plotting only

--- bin/testing_generate_regrid_product.py
+++ bin/testing_generate_regrid_product.py
@@ -19,19 +19,19 @@ test_f1 = f'{root}/test_files/20130909_mace-head_ecmwf-exp0001-0-11.nc'
 test_f2 = f'{root}/test_files/20130909_mace-head_ecmwf-exp0001-12-23.nc'
 
 # Run all product with all test_model files
-for product, oname in zip(['iwc', 'lwc', 'cv'],[iwc_name, lwc_name, cv_name]):
-    generate_regrid_products('ecmwf', product, [fname, test_f1, test_f2], oname)
+#for product, oname in zip(['iwc', 'lwc', 'cv'],[iwc_name, lwc_name, cv_name]):
+#    generate_regrid_products('ecmwf', product, [fname, test_f1, test_f2], oname)
 
-data1 = netCDF4.Dataset(lwc_name).variables['ecmwf_lwc'][:]
+data1 = netCDF4.Dataset(iwc_name).variables['ecmwf_iwc'][:]
 data1[data1 <= 0] = ma.masked
-data2 = netCDF4.Dataset(lwc_name).variables['lwc_obs_ecmwf'][:]
+data2 = netCDF4.Dataset(iwc_name).variables['iwc_obs_ecmwf'][:]
 time = netCDF4.Dataset(lwc_name).variables['time'][:]
 height = netCDF4.Dataset(lwc_name).variables['ecmwf_height'][:]
 
-lwc_file = '/home/korpinen/Documents/ACTRIS/cloudnet_products/tests/source_data/lwc.nc'
-lwc_data = netCDF4.Dataset(lwc_file).variables['lwc'][:]
-lwc_time = netCDF4.Dataset(lwc_file).variables['time'][:]
-lwc_height = netCDF4.Dataset(lwc_file).variables['height'][:]
+iwc_file = f'{root}/test_files/iwc.nc'
+lwc_data = netCDF4.Dataset(iwc_file).variables['iwc'][:]
+lwc_time = netCDF4.Dataset(iwc_file).variables['time'][:]
+lwc_height = netCDF4.Dataset(iwc_file).variables['height'][:]
 
 plot_2d(data1)
 plot_2d(data2)
