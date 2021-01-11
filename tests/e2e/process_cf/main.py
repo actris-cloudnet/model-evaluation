34aadfb1c8b593d167f186c8cd242a319316cd8d
Anniina Korpinen <anniina.korpinen@fmi.fi>
2021-01-11 16:53:32 +0200

    Adds end2end tests for cloud fraction processing

---
+++ tests/e2e/process_cf/main.py
@@ -0,0 +1,40 @@
+import sys
+import subprocess
+from os import path
+import argparse
+from tempfile import TemporaryDirectory
+
+ROOT_PATH = ''
+for p in sys.path:
+    if 'model_evaluation' in p and 'test' not in p:
+        ROOT_PATH = p
+
+sys.path.append(f'{ROOT_PATH}/model_evaluation/products/')
+process_day_evaluation = __import__("product_resampling")
+SCRIPT_PATH = path.dirname(path.realpath(__file__))
+test_file_model = f'{ROOT_PATH}/test_files/20190517_mace-head_ecmwf.nc'
+test_file_product = f'{ROOT_PATH}/test_files/categorize.nc'
+
+
+def _process():
+    tmp_dir = TemporaryDirectory()
+    temp_file = f'{tmp_dir.name}/xx.nc'
+    process_day_evaluation.resample_observation2model('ecmwf', 'cf', [test_file_model],
+                               test_file_product, temp_file)
+
+    try:
+        subprocess.call(['pytest', '-v', f'{SCRIPT_PATH}/tests.py', '--full_path', temp_file])
+    except subprocess.CalledProcessError:
+        raise
+
+    tmp_dir.cleanup()
+
+
+def main():
+    _process()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Model evaluation Cloud fraction processing e2e test.')
+    ARGS = parser.parse_args()
+    main()
